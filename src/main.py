44adbab7da7d205cc0e3cb7da719f21cc6301d14
Raveesh Vyas <raveeshvyas1410@gmail.com>
2025-12-05 19:12:01 +0530

    Added option to load .env file

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import os
 from src.elasticsearch_client import ElasticsearchClient
 from src.simulation_client import SimulationClient
 from src.logger import setup_logger, log_interaction
+from dotenv import load_dotenv
 
 from src.features import FeatureExtractor
 from src.ranker import PersonalizedRanker
@@ -13,6 +14,7 @@ from src.mf_ranker import MFRanker
 from src.bpr_ranker import BPRRanker
 from src.linucb_ranker import LinUCBRanker
 
+load_dotenv()
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
